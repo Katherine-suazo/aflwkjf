867b01d21375a772573a7d8da92b5c5043439ce3
Katherine <katherinesuazo032@gmail.com>
2025-04-24 12:34:23 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+document.write("<h1> Hola JavaScript </h1>")
+// comentarios de una linea
+/* Comentarios de bloque */
+
+let nombre="Carlos"
+console.log(nombre)
+
+// Array
+
+let datos=["Ana", "Pablo", "Carlos"]
+
+let edades=[23, 17, 44, 15]
+
+let objetoDatos={
+    nombre:"Ana",
+    edad:25,
+    cuenta:"Alumno",
+    notas:[5, 5, 6.8, 5.9]
+}
+
+console.log(datos)
+console.log(edades)
+console.log(objetoDatos)
